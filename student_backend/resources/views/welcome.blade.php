60646f1d23d20e518d21b6cc32f05e358512803f
Salem Moqa <moqasalem@gmail.com>
2023-01-07 23:40:03 +0300

    upload

---
+++ student_backend/resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1,
+  maximum-scale=5" />
+
+ 
+  
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+
+  <title>{{ config('app.name')}}</title>
+  
+</head>
+
+<body>
+
+
+
+  
+
+
+ 
+
+ 
+</body>
+</html>
